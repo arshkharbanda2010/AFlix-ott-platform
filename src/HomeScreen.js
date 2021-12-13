07464bc95f6a1744fa26e4b0a7933c5278e9ccc5
arshkharbanda2010 <arsh.kharbanda2010@gmail.com>
2021-12-13 14:25:06 +0530

    navbar hide functionality

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,6 +6,25 @@ function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
       {/* banner */}
       {/* tiles */}
     </div>
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./Nav.css";
 function Nav() {
+  const [show, handleShow] = useState(false);
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+      console.log("ture");
+    } else {
+      handleShow(false);
+      console.log("false");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+    return () => window.removeEventListener("scroll", transitionNavbar);
+  }, []);
   return (
-    <div className="nav nav__black">
+    <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img
           className="nav__logo"
