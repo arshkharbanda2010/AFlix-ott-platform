ee2bd8edccf12ec621ece98bc024a0b59e531caa
arshkharbanda2010 <arsh.kharbanda2010@gmail.com>
2021-12-13 13:20:14 +0530

    navbar added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDIxw2medZ0ESoLVS9Ao3vKTLYMurFbVFY",
+  authDomain: "netflix-clone-3f663.firebaseapp.com",
+  projectId: "netflix-clone-3f663",
+  storageBucket: "netflix-clone-3f663.appspot.com",
+  messagingSenderId: "108326501331",
+  appId: "1:108326501331:web:3890af37a9280626954d26",
+};
