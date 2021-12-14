93376b944ae3303f0c00370488dd5aa24634e4f9
arshkharbanda2010 <arsh.kharbanda2010@gmail.com>
2021-12-14 18:24:31 +0530

    login page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import "./LoginScreen.css";
 
 function LoginScreen() {
+  const [signIn, setsignIn] = useState(false);
   return (
     <div className="loginScreen">
       <div className="loginScreen__background">
@@ -14,7 +16,20 @@ function LoginScreen() {
 
         <div className="loginScreen__gradient" />
 
-        <div className="loginScreen__body"></div>
+        <div className="loginScreen__body">
+          <h1>Unlimited films, TV programmes and more</h1>
+          <h2>Watch anywhere. Cancel at any time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="loginScreen__input">
+            <form action="">
+              <input type="email" placeholder="Email address" />
+              <button className="loginScreen__getStarted">Get Started</button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
